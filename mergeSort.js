92440c5049fc4adb8663e6b430f5274a0e1b2c62
Ralph Rabelo <ralphrabelo@gmail.com>
2023-08-01 06:52:33 -0300

    criação do mergeSort

---
+++ mergeSort.js
@@ -0,0 +1,60 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  // Split the array into two halves
+  const middle = Math.floor(arr.length / 2);
+  const leftHalf = arr.slice(0, middle);
+  const rightHalf = arr.slice(middle);
+
+  // Recursively sort the left and right halves
+  const sortedLeft = mergeSort(leftHalf);
+  const sortedRight = mergeSort(rightHalf);
+
+  // Merge the sorted halves
+  return merge(sortedLeft, sortedRight);
+}
+
+function merge(left, right) {
+  let mergedArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  // Compare elements from both arrays and add the smaller one to the mergedArray
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      mergedArray.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      mergedArray.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  // Add any remaining elements from the left and right arrays
+  while (leftIndex < left.length) {
+    mergedArray.push(left[leftIndex]);
+    leftIndex++;
+  }
+
+  while (rightIndex < right.length) {
+    mergedArray.push(right[rightIndex]);
+    rightIndex++;
+  }
+
+  return mergedArray;
+}
+
+// Example usage:
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = mergeSort(unsortedArray);
+console.log(sortedArray); // Output: [11, 12, 22, 25, 34, 64, 90]
+//This implementation of merge sort will sort the input array in ascending order. 
+//The mergeSort function recursively divides the array into smaller parts and 
+//Then uses. The merge function to combine them back into a sorted array. 
+
+
+
+
+
