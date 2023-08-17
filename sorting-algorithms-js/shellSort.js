520e728fc1f786b05f3ee5bda32eb543167eccb6
Ralph Rabelo <ralphrabelo@gmail.com>
2023-08-17 07:37:52 -0300

    configuring tool-versions

--- sorting-algorithms-js/shellSort.js
+++ sorting-algorithms-js/shellSort.js
@@ -1,28 +1,39 @@
 /** 
  * Shell sort is an extension of the insertion sort algorithm. It starts by sorting pairs of elements far apart from each other and then progressively reduces the gap between elements to be compared. Here's a JavaScript implementation of Shell sort: 
  */
-export function shellSort(arr) {
-    const n = arr.length;
-    let gap = Math.floor(n / 2);
-  
-    while (gap > 0) {
-      for (let i = gap; i < n; i++) {
-        let temp = arr[i];
-        let j = i;
-  
-        while (j >= gap && arr[j - gap] > temp) {
-          arr[j] = arr[j - gap];
-          j -= gap;
+export function shellSort(arr)
+{
+    let n = arr.length;
+   
+        // Start with a big gap, then reduce the gap
+        for (let gap = Math.floor(n/2); gap > 0; gap = Math.floor(gap/2))
+        {
+          
+            // Do a gapped insertion sort for this gap size.
+            // The first gap elements a[0..gap-1] are already
+            // in gapped order keep adding one more element
+            // until the entire array is gap sorted
+            for (let i = gap; i < n; i += 1)
+            {
+              
+                // add a[i] to the elements that have been gap
+                // sorted save a[i] in temp and make a hole at
+                // position i
+                let temp = arr[i];
+   
+                // shift earlier gap-sorted elements up until
+                // the correct location for a[i] is found
+                let j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                    arr[j] = arr[j - gap];
+   
+                // put temp (the original a[i]) in its correct
+                // location
+                arr[j] = temp;
+            }
         }
-  
-        arr[j] = temp;
-      }
-  
-      gap = Math.floor(gap / 2);
-    }
-  
-    return arr;
-  }
+        return arr;
+}
 
 /*
 // Example usage:
---
+++ sorting-algorithms-python/__main__.py
@@ -0,0 +1,14 @@
+from shellSort import shellSort
+
+def __main__():
+    # driver to check the code
+    arr2 = [12, 34, 54, 2, 3]
+    print("input array:",arr2)
+
+    shellSort(arr2,len(arr2))
+    print("sorted array",arr2)
+
+    # This code is contributed by Illion
+
+if __name__ == "__main__":
+    __main__()
